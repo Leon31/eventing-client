eafe67874b956e0ded661f184d7b230e0fe7a193
Leonardo Di Vittorio <leonardo.divittorio@live.it>
2018-07-07 17:18:39 +0200

    cr app basic - linter setup

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+
+module.exports =   {
+    "parser": "babel-eslint",
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "amd":true,
+    },
+    "extends": ["plugin:react/recommended"],
+    "parserOptions": {
+        "ecmaVersion":8,
+        "ecmaFeatures": {
+            "ecmaVersion":2017,
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+    ],
+    "rules": {
+        "react/prop-types": 0,
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
